230dc69f507595095db2c30f0c83c8590dd42a15
Turmo11 <Turmo11@users.noreply.github.com>
2019-10-29 18:53:00 +0100

    Initial commit
    Core and container files added as well as some basic module (window, audio, render, input, textures)

---
+++ WesternPepsiPinball/WesternPepsiPinball/Application.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "p2List.h"
+#include "Globals.h"
+
+class Module;
+class ModuleRender;
+class ModuleWindow;
+class ModuleTextures;
+class ModuleInput;
+class ModuleAudio;
+
+
+
+class Application
+{
+public:
+	ModuleRender* renderer;
+	ModuleWindow* window;
+	ModuleTextures* textures;
+	ModuleInput* input;
+	ModuleAudio* audio;
+
+private:
+
+	p2List<Module*> list_modules;
+
+public:
+
+	Application();
+	~Application();
+
+	bool Init();
+	update_status Update();
+	bool CleanUp();
+
+private:
+
+	void AddModule(Module* mod);
+};
